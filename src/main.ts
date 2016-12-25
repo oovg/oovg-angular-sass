b29818e25d89335f8cbf215aad63ccd55859ce35
oovg <office@vengist.com>
2016-12-25 11:04:05 -0800

    fixes typescript semantic error

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 // Libraries
+///<reference path="../node_modules/angular2/platform/browser.d.ts"/>
 import {bootstrap}        from 'angular2/platform/browser'
 import {ROUTER_PROVIDERS} from 'angular2/router';
 
 // Custom Components
 import {AppComponent}     from './component/app/app'
+
 
 bootstrap(AppComponent, [
 	ROUTER_PROVIDERS
